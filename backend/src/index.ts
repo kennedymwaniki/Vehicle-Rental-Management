195836e3ac3b001f322ca7fdff08c1c27d08bdf3
kennedymwaniki <kennedymwaniki089@gmail.com>
2024-07-02 23:45:49 +0300

    cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,12 +8,20 @@ import { paymentsRouter } from "./payments/paymentsRouter";
 import { vehicleRouter } from "./vehicles/vehicleRouter";
 import { vehicleSpecsRouter } from "./vehiclespecs/vehicleSpecsRouter";
 import { customerSupportRouter } from "./customerSupport/customerSupportRouter";
-
+import { cors } from "hono/cors";
 const app = new Hono();
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
+
+app.use(
+  cors({
+    origin: "*", // specify your frontend URL
+    allowMethods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
 app.route("/api", userRouter);
 app.route("/api", bookingRouter);
 app.route("/api", fleetRouter);
