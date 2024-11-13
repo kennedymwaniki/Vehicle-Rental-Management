b7e355bf8a65f9ed70476f38a6b83306c88d0a14
kennedymwaniki <kennedymwaniki089@gmail.com>
2024-11-13 15:04:30 +0300

    404 page

--- client/src/components/Unauthorized.tsx
+++ client/src/components/Unauthorized.tsx
@@ -9,16 +9,39 @@ const Unauthorized = () => {
     // Redirect to the homepage after 3 seconds
     const timer = setTimeout(() => {
       navigate("/");
-    }, 3000);
+    }, 4000);
 
     // Cleanup the timer on component unmount
     return () => clearTimeout(timer);
   }, [navigate]);
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold mb-4">Unauthorized Access</h1>
-      <p className="text-lg">You will be redirected to the homepage shortly.</p>
+    <div className="text-center">
+      <h1 className="mb-4 text-6xl font-semibold text-red-500">
+        You are not authorized to view this page
+        <span>❌</span>
+      </h1>
+      <p className="mb-4 text-lg text-gray-600">
+        Oops! Looks like you're lost.
+      </p>
+      <div className="animate-bounce">
+        <svg
+          className="mx-auto h-16 w-16 text-red-500"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
+          ></path>
+        </svg>
+      </div>
+      <p className="mt-4 text-gray-600">
+        Let's get you back <p className="text-blue-500">home</p>.
+      </p>
     </div>
   );
 };
